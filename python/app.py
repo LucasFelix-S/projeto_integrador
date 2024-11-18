a6ead7eadbb745ab9c6e7e50551431bbed51eddc
Lucas Félix Santos <started.lucasfelixsantos@gmail.com>
2024-11-18 20:23:33 -0300

    atualização de teste para o ambiente de produção

--- python/app.py
+++ python/app.py
@@ -65,4 +65,4 @@ def login():
         return jsonify({"error": "Erro ao processar o login."}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True,host='0.0.0.0', port=5000)
